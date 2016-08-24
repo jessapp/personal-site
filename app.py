7ff1f0a1e9a91bc245842ad53cc5d4f13c4e94e6
Jessica Appelbaum <jess.appelbaum@gmail.com>
2016-08-24 15:55:43 -0400

    Set up site directory

--- app.py
+++ app.py
@@ -5,12 +5,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello world!"
+	return render_template(
+		'home.html',**locals())
 
 @app.route("/bio")
 def bio():
 	return render_template(
 		'bio.html',**locals())
+
+@app.route("/video")
+def video():
+	return render_template(
+		'video.html',**locals())
+
+@app.route("/programming")
+def programming():
+	return render_template(
+		'programming.html',**locals())
+
+@app.route("/contact")
+def contact():
+	return render_template(
+		'contact.html',**locals())
 
 if __name__ == "__main__":
 	app.run()
